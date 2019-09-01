cb0d73f3940b02e4ae7f26a2423611bd0d67e809
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-09-02 02:02:16 +0500

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,42 @@
+# tkinter Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# The example we'll use is a simple GUI tool that will convert a number. #
+# of feet to the equivalent number of meters.  If we were to sketch this #
+# out, it might look something like this:                                #
+#                  #######################################               #
+#                  #           feet to meters            #               #
+#                  #######################################               #
+#                  #                        _____ feet   #               #
+#                  #      is equivalent to  _____ meters #               #
+#                  #                                     #               #
+#                  #                       ############# #               #
+#                  #                       # calculate # #               # 
+#                  #                       ############# #               #
+#                  #######################################               #
+# So it looks like we have a short text entry widget that will let us    #
+# type in the number of feet, and a 'Calculate' button that will get the #
+# value out of that entry, perform the calculation, and then put the     #
+# resulting number of meters on the screen just below where the entry    #
+# is. We've also got three static labels ("feet", "is equivalent to",    #
+# and "meters") which help our user figure out how to use the interface. #
+# In terms of layout, things seem to naturally divide into three columns #
+# and three rows:                                                        #
+#                  ############|############|###############             #
+#                  #           |feet to mete|rs            #             #
+#                  ############|############|###############             #
+#                  -----------------------------------------             #
+#                  #           |            | _____ feet   #             #
+#                  #      is eq|uivalent to | _____ meters #             #
+#                  #           |            |              #             #
+#                  -----------------------------------------             #
+#                  #           |            |############# #             #
+#                  #           |            |# calculate # #             #  
+#                  #           |            |############# #             #
+#                  ############|############|###############             #
+##########################################################################
+
+# your code goes here
